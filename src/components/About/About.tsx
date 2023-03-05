a2a49d888d728fd79463189e392ac58cca1aa797
Adib Ahsan Chowdhury <adibahsan@iut-dhaka.edu>
2023-03-06 00:41:31 +0600

    About section added + meta tags changed

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -19,20 +19,28 @@ export function About(){
         <ScrollAnimation animateIn="fadeInLeft">
           <h2>About Me</h2>
         </ScrollAnimation>
+
         <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000}>
-          <p>Me chamo João Túlio, sou técnico em informática com uma grande paixão por programação. Sou apaixonado por entregar soluções que agregam na vida das pessoas e ao mesmo tempo me desafiam. Aprimorei minhas habilidades como desenvolvedor Front-End e Back-End</p>
+          <p>Hi, I’m Adib and I’m a full-stack web developer who loves building innovative and user-friendly solutions.</p>
         </ScrollAnimation>
+
+        <ScrollAnimation animateIn="fadeInLeft" style={{marginTop: "2rem", marginBottom: "2rem"}} delay={0.3 * 1000}>
+
+          <p>I have four years of experience in development and have worked on projects such as real-time delivery systems and e-commerce platforms using Spring Boot, React, and React Native. I also like leading and coaching teams of software engineers and interns, as well as supporting the designers and UX team with my graphics designing skills in Photoshop and illustrator.</p>
+        </ScrollAnimation>
+
+
         <ScrollAnimation animateIn="fadeInLeft" delay={0.4 * 1000} style={{marginTop: "2rem", marginBottom: "2rem"}}>
 
-          <p>Desenvolvo sites e aplicações utilizando HTML, CSS, e JavaScript. Tenho familiaridade em desenvolver layouts que me fornecem. Estou sempre me aprimorando a cada projeto que colocam em minhas mãos.</p>
+          <p>My aim is to use my skills and expertise to deliver high-quality products that satisfy the needs of my clients and users. I am always keen to learn new technologies and tools that can improve my work.</p>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInLeft" delay={0.6 * 1000}>
 
-          <p>Sou uma pessoa dedicada que corre atrás para realizar seus sonhos, trabalhador e orientado a resultados, sempre busco alcançar minha melhor versão.</p>
+          <p>I’m looking for new opportunities to work with other professionals who share my passion. If you want to work with me or know more about my projects, don’t hesitate to connect with me or send me a message.</p>
         </ScrollAnimation>
 
         <ScrollAnimation animateIn="fadeInLeft" delay={0.7 * 1000}>
-          <h3>Aqui estão as minhas principais habilidades:</h3>
+          <h3>Here are my technical skills:</h3>
         </ScrollAnimation>
         <div className="hard-skills">
 
