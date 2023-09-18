7c07499b24a0af39bc9d67a527b4a67f9383bf3e
Adib <adibahsan@iut-dhaka.edu>
2023-09-18 23:58:36 +0600

    crash on CI/CD pipeline fixed

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev";
 
 ReactDOM.render(
     <React.StrictMode>
-        <DevSupport ComponentPreviews={ComponentPreviews}
-                    useInitialHook={useInitial}
-        >
             <App/>
-        </DevSupport>
     </React.StrictMode>,
     document.getElementById('root')
 )
