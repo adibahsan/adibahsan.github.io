ee6a018672841da4df6a59688fb3972ce1fecf19
Adib <adibahsan@iut-dhaka.edu>
2023-09-19 01:24:47 +0600

    recaptcha disabled

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -76,12 +76,12 @@ export function Form() {
           field="message"
           errors={state.errors}
         />
-        <ReCAPTCHA
-          sitekey="6LcAu-IdAAAAAJOTI5E_eRltZNQCvukIl2-f1glQ"
-          onChange={(e) => {
-            setIsHuman(true)
-          }}
-        ></ReCAPTCHA>
+        {/*<ReCAPTCHA*/}
+        {/*  sitekey="6LcAu-IdAAAAAJOTI5E_eRltZNQCvukIl2-f1glQ"*/}
+        {/*  onChange={(e) => {*/}
+        {/*    setIsHuman(true)*/}
+        {/*  }}*/}
+        {/*></ReCAPTCHA>*/}
         <button
           type="submit"
           disabled={state.submitting || !validEmail || !message || !isHuman}
