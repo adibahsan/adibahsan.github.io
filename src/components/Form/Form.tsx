3d78856a455795fe6b7ad25a5507928d11902836
Adib <adibahsan@iut-dhaka.edu>
2024-02-07 01:47:47 +0600

    form submission updated

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,50 +1,50 @@
-import { Container, ContainerSucces } from './styles'
-import { useForm, ValidationError } from '@formspree/react'
-import { toast, ToastContainer } from 'react-toastify'
-import ReCAPTCHA from 'react-google-recaptcha'
-import { useEffect, useState } from 'react'
-import validator from 'validator'
+import { Container, ContainerSucces } from "./styles";
+import { useForm, ValidationError } from "@formspree/react";
+import { toast, ToastContainer } from "react-toastify";
+import ReCAPTCHA from "react-google-recaptcha";
+import { useEffect, useState } from "react";
+import validator from "validator";
 
 export function Form() {
-  const [state, handleSubmit] = useForm('myyozglw')
+  const [state, handleSubmit] = useForm("mzbnekdk");
 
-  const [validEmail, setValidEmail] = useState(false)
-  const [isHuman, setIsHuman] = useState(false)
-  const [message, setMessage] = useState('')
+  const [validEmail, setValidEmail] = useState(false);
+  const [isHuman, setIsHuman] = useState(false);
+  const [message, setMessage] = useState("");
 
   function verifyEmail(email: string) {
     if (validator.isEmail(email)) {
-      setValidEmail(true)
+      setValidEmail(true);
     } else {
-      setValidEmail(false)
+      setValidEmail(false);
     }
   }
 
   useEffect(() => {
     if (state.succeeded) {
-      toast.success('Email enviado com sucesso!', {
+      toast.success("Email Sent Successfully", {
         position: toast.POSITION.BOTTOM_LEFT,
         pauseOnFocusLoss: false,
         closeOnClick: true,
         hideProgressBar: false,
-        toastId: 'succeeded',
-      })
+        toastId: "succeeded",
+      });
     }
-  })
+  });
   if (state.succeeded) {
     return (
       <ContainerSucces>
-        <h3>Obrigado por entrar em contato!</h3>
+        <h3>Thank you for contacting me.</h3>
         <button
           onClick={() => {
-            window.scrollTo({ top: 0, behavior: 'smooth' })
+            window.scrollTo({ top: 0, behavior: "smooth" });
           }}
         >
-          Voltar ao topo
+          Back To Top
         </button>
         <ToastContainer />
       </ContainerSucces>
-    )
+    );
   }
 
   return (
@@ -57,7 +57,7 @@ export function Form() {
           type="email"
           name="email"
           onChange={(e) => {
-            verifyEmail(e.target.value)
+            verifyEmail(e.target.value);
           }}
           required
         />
@@ -68,7 +68,7 @@ export function Form() {
           id="message"
           name="message"
           onChange={(e) => {
-            setMessage(e.target.value)
+            setMessage(e.target.value);
           }}
         />
         <ValidationError
@@ -76,20 +76,21 @@ export function Form() {
           field="message"
           errors={state.errors}
         />
-        {/*<ReCAPTCHA*/}
-        {/*  sitekey="6LcAu-IdAAAAAJOTI5E_eRltZNQCvukIl2-f1glQ"*/}
-        {/*  onChange={(e) => {*/}
-        {/*    setIsHuman(true)*/}
-        {/*  }}*/}
-        {/*></ReCAPTCHA>*/}
+        <ReCAPTCHA
+          sitekey="6Ldes2kpAAAAABK86jwj_gQ5SS9YuNB2dIagIr8L"
+          onChange={(e) => {
+            console.log("recaptcha", e);
+            setIsHuman(true)
+          }}
+        />
         <button
           type="submit"
-          disabled={state.submitting || !validEmail || !message || !isHuman}
+          disabled={state.submitting || !validEmail || !message }
         >
           Send
         </button>
       </form>
       <ToastContainer />
     </Container>
-  )
+  );
 }
