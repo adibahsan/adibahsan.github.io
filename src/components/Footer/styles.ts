38c0720a2601fb741c9f7641bc6a0b3a00ad4a6f
Adib <adibahsan@iut-dhaka.edu>
2024-02-17 03:47:39 +0600

    more alignment fixes

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 export const Container = styled.footer`
     background-color: #2b2b2b;
-    padding: 3rem 7rem;
+    padding: 3rem 9rem;
     margin-top: 15rem;
     display: flex;
     align-items: center;
