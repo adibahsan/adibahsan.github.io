3dcaed3c3cc06b5543effd41800b6f38ec361731
Adib <adibahsan@iut-dhaka.edu>
2024-02-18 03:44:45 +0600

    about section responsiveness refactored

--- src/components/About/styles.ts
+++ src/components/About/styles.ts
@@ -1,78 +1,86 @@
 import styled from "styled-components";
 
 export const Container = styled.section`
-  margin-top: 12rem;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 2rem;
+    margin-top: 12rem;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 2rem;
 
-  .hard-skills{
-    margin-top: 1.6rem;
-    display: flex;
-    align-items: center;
-    flex-wrap: wrap;
-    gap: 1.8rem;
-  }
-  .hability{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    img{
-      width: 3.4rem;
+    .hard-skills {
+        margin-top: 1.6rem;
+        display: flex;
+        align-items: center;
+        flex-wrap: wrap;
+        gap: 1.8rem;
     }
-  }
 
-  h2{
-    display: inline-block;
-    margin-bottom: 2rem;
-    border-bottom: 0.2rem solid var(--blue);
-  }
+    .hability {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
 
-  h3{
-    margin-top: 2rem;
-    color: var(--green);
-  }
+        img {
+            width: 3.4rem;
+        }
+    }
 
-  p{
-    font-size: 1.8rem;
-    letter-spacing: 0.1rem;
-    font-weight: 500;
-  }
-  
-  
+    h2 {
+        display: inline-block;
+        margin-bottom: 2rem;
+        border-bottom: 0.2rem solid var(--blue);
+    }
 
-  .about-image{
-    text-align: center;
-   img{
-     margin-top: 2rem;
-     width: 100%;
-     filter: grayscale(1);
-     transition: filter 0.5s;
-     &:hover{
-       filter: grayscale(0);
-     }
-   }
-  }
+    h3 {
+        margin-top: 2rem;
+        color: var(--green);
+    }
 
-  @media only screen and (max-width: 480px) {
+    p {
+        font-size: 1.8rem;
+        letter-spacing: 0.1rem;
+        font-weight: 500;
+    }
+
+
     .about-image {
-      max-width: 100%;
-      margin-top: 4rem;
-    }
-  }
+        text-align: center;
 
-  @media (max-width: 960px){
-    display: block;
-    text-align: center;
+        img {
+            margin-top: 2rem;
+            width: 100%;
+            filter: grayscale(1);
+            transition: filter 0.5s;
 
-    .about-image{
-      display: flex;
+            &:hover {
+                filter: grayscale(0);
+            }
+        }
     }
-    .hard-skills{
-      justify-content: center;
+
+    @media only screen and (max-width: 480px) {
+        .about-image {
+            max-width: 100%;
+        }
+    }
+
+    @media (max-width: 960px) {
+        display: flex;
+        flex-direction: column-reverse;
+        text-align: center;
+        margin-top: 6rem;
+
+        .about-image {
+            display: flex;
+        }
+
+        .hard-skills {
+            justify-content: center;
+        }
+
     }
     
-  }
-
+    @media (max-width: 600px) {
+        margin-top: 3rem;
+    }
+    
 `
