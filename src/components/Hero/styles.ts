a754a96f2a01030c6c0a2be5f13296c7747a3359
Adib <adibahsan@iut-dhaka.edu>
2024-02-18 03:30:36 +0600

    center alignment on the "Hero" section for mobile view

--- src/components/Hero/styles.ts
+++ src/components/Hero/styles.ts
@@ -1,63 +1,68 @@
 import styled from "styled-components";
 
 
-
 export const Container = styled.section`
-  padding-top: 15%;
-  display: flex;
-  justify-content: space-between;
-  gap: 8rem;
-  background: rgba(0,0,0,0);
-  .hero-text{
-    & > p{
-      font-size: 1.8rem;
+    padding-top: 15%;
+    display: flex;
+    justify-content: space-between;
+    gap: 8rem;
+    background: rgba(0, 0, 0, 0);
+
+    .hero-text {
+        & > p {
+            font-size: 1.8rem;
+        }
+
+        h1 {
+            font-size: 7rem;
+        }
+
+        h3 {
+            color: var(--green);
+            margin: 1rem 0;
+        }
+
+
+        p.small-resume {
+            margin-bottom: 5rem;
+        }
     }
-    h1{
-      font-size: 7rem;
+
+    .button {
+        margin-top: 5rem;
+        padding: 1.4rem 6rem;
     }
 
-    h3{
-      color:var(--green);
-      margin: 1rem 0;
+    .hero-image {
+        img {
+            max-width: 500px;
+        }
     }
 
-    
-    p.small-resume {
-      margin-bottom: 5rem;
+
+    @media (max-width: 960px) {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
+
+        margin-top: 15%;
+        .hero-text {
+
+            h1 {
+                font-size: 5rem;
+            }
+        }
+
+        .hero-image {
+            display: none;
+        }
     }
-  }
 
-  .button{
-    margin-top: 5rem;
-    padding: 1.4rem 6rem;
-  }
-
-  .hero-image{
-    img{
-      max-width: 500px;
+    @media (max-width: 600px) {
+        margin-top: 25%;
     }
-  }
-
-
-  @media(max-width: 960px){
-    display: block;
-    margin-top: 15%;
-    .hero-text{
-
-      h1{
-        font-size: 5rem;
-      }
+    @media (max-width: 480px) {
+        margin-top: 35%;
     }
-    
-    .hero-image{
-      display: none;
-    }
-  }
-
-  @media(max-width: 600px){
-    margin-top: 25%;
-  }
-  @media(max-width: 480px){
-    margin-top: 35%;
-  }
 `
